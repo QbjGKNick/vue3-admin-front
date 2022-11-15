eb248187b93504c580ff68f28dd039e3701e471f
江前彪 <jiangqb@jiangqianbiaos-MacBook-Pro.local>
2022-11-15 22:51:47 +0800

    feat: add pinia

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
-<script setup lang="ts"></script>
-
 <template>
   <router-link to="/home">首页</router-link>
   <router-link to="/about">关于</router-link>
+
   <router-view></router-view>
+
+  <Counter></Counter>
 </template>
+
+<script setup lang="ts">
+import Counter from "@/components/Counter/index.vue"
+</script>
