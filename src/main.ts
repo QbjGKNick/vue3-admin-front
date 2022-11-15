ac0f89b6bd58e8a30bd58bec63c5e2434c9fb541
江前彪 <jiangqb@jiangqianbiaos-MacBook-Pro.local>
2022-11-15 23:21:14 +0800

    feat: add style 样式

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import router from "./router/index"
 import { createPinia } from "pinia"
 import ElementPlus from "element-plus"
 import "element-plus/dist/index.css"
+// 初始化css，重置 css 默认样式
+import "normalize.css/normalize.css"
+// 全局 css
+import "@/styles/index.scss"
 
 const app = createApp(App)
 app.use(ElementPlus)
