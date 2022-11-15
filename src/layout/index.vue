cd8b527a31de764af2004226e961b4fa8fbc57fb
江前彪 <jiangqb@jiangqianbiaos-MacBook-Pro.local>
2022-11-15 23:24:59 +0800

    feat: 完善layout样式

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -13,3 +13,32 @@
     </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.app-wrapper {
+  display: flex;
+  width: 100%;
+  height: 100%;
+  .main-container {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    .header {
+      background: cyan;
+      .navbar {
+        height: 50px;
+        background: #1890ff;
+      }
+      .tags-view {
+        height: 34px;
+        background: #12efff;
+      }
+    }
+    .app-main {
+      /* main = 100% - navbar + tagsview */
+      min-height: calc(100vh - 84px);
+      background: red;
+    }
+  }
+}
+</style>
