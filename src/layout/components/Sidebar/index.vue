f40d1bf9c88efef0f48de7da320072cc152a33e7
jiangqb <jiangqb@citycloud.com.cn>
2022-12-03 16:24:32 +0800

    feat(logo): 添加logo并控制显示隐藏

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -2,31 +2,35 @@
  * @Author: jiangqb jiangqb@citycloud.com.cn
  * @Date: 2022-11-25 19:30:18
  * @LastEditors: jiangqb jiangqb@citycloud.com.cn
- * @LastEditTime: 2022-12-03 16:17:21
+ * @LastEditTime: 2022-12-03 16:20:55
  * @FilePath: /vue3-admin-front/src/layout/components/Sidebar/index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
   <!-- <h4 @click="isCollapse = !isCollapse">展收测试</h4> -->
-  <logo v-if="showLogo" :collapse="isCollapse" />
-  <el-menu
-    class="sidebar-container-menu"
-    mode="vertical"
-    :default-active="activeMenu"
-    :background-color="scssVariables.menuBg"
-    :text-color="scssVariables.menuText"
-    :active-text-color="themeColor"
-    :collapse="sidebar.opened"
-    :collapse-transition="true"
-  >
-    <!-- 循环sidebar-item 组件 -->
-    <sidebar-item
-      v-for="route in menuRoutes"
-      :key="route.path"
-      :item="route"
-      :base-path="route.path"
-    />
-  </el-menu>
+  <div class="sidebar-wrapper">
+    <logo v-if="showLogo" :collapse="isCollapse" />
+    <scroll-panel>
+      <el-menu
+        class="sidebar-container-menu"
+        mode="vertical"
+        :default-active="activeMenu"
+        :background-color="scssVariables.menuBg"
+        :text-color="scssVariables.menuText"
+        :active-text-color="themeColor"
+        :collapse="sidebar.opened"
+        :collapse-transition="true"
+      >
+        <!-- 循环sidebar-item 组件 -->
+        <sidebar-item
+          v-for="route in menuRoutes"
+          :key="route.path"
+          :item="route"
+          :base-path="route.path"
+        />
+      </el-menu>
+    </scroll-panel>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -63,4 +67,14 @@ const activeMenu = computed(() => {
 const showLogo = computed(() => settingStore.settings.sidebarLogo)
 // 展开收起状态 稍后放 store 当前是展开就让它收起
 const isCollapse = computed(() => store.sidebar.opened)
+
+const logoHeight = computed(() => (showLogo.value ? 50 : 0) + "px")
 </script>
+
+<style lang="scss" scoped>
+.sidebar-wrapper {
+  .sidebar-container-menu {
+    height: calc(100vh - v-bind(logoHeight));
+  }
+}
+</style>
