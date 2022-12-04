1f733c0f57a0217d406ece4b80d58d3a441a84be
江前彪 <jiangqb@jiangqianbiaos-MacBook-Pro.local>
2022-12-04 20:45:59 +0800

    feat: 错误码 401 token 失效处理

--- src/api/config/request.ts
+++ src/api/config/request.ts
@@ -1,6 +1,7 @@
-import axios from "axios"
+import axios, { AxiosError } from "axios"
 import { getToken } from "@/utils/auth"
 import { ElMessage } from "element-plus"
+import { useUserStore } from "@/stores/user"
 
 const service = axios.create({
   baseURL: import.meta.env.VITE_BASE_API,
@@ -22,14 +23,27 @@ service.interceptors.request.use(
   }
 )
 
-service.interceptors.response.use((response) => {
-  const { code, message } = response.data
-  if (code !== 0) {
-    // 错误提示
-    ElMessage.error(message)
-    return Promise.reject(message)
+service.interceptors.response.use(
+  (response) => {
+    const { code, message } = response.data
+    if (code !== 0) {
+      // 错误提示
+      ElMessage.error(message)
+      return Promise.reject(message)
+    }
+    return response.data
+  },
+  (error: AxiosError) => {
+    const res = error?.response
+    if (res && res.status === 401) {
+      // 401 未登录 token 失效
+      const store = useUserStore()
+      store.resetToken()
+      window.location.reload()
+    }
+    ElMessage.error(error.message)
+    return Promise.reject(error)
   }
-  return response.data
-})
+)
 
 export default service
--- src/stores/user.ts
+++ src/stores/user.ts
@@ -2,7 +2,7 @@
  * @Author: error: git config user.name && git config user.email & please set dead value or install git
  * @Date: 2022-12-04 19:44:28
  * @LastEditors: error: git config user.name && git config user.email & please set dead value or install git
- * @LastEditTime: 2022-12-04 20:34:11
+ * @LastEditTime: 2022-12-04 20:41:12
  * @FilePath: /vue3-admin-front/src/stores/user.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -32,13 +32,16 @@ export const useUserStore = defineStore("user", () => {
     }
   }
   const { delAllView } = useTagsView()
-  const logout = () => {
+  const resetToken = () => {
     // 清空store里token
     state.token = ""
     // 清空localStorage里的token
     removeToken()
+  }
+  const logout = () => {
+    resetToken()
     // 清除所有tag views
     delAllView()
   }
-  return { state, login, logout }
+  return { state, login, logout, resetToken }
 })
